7adc0cf0736abfb40e3c7ff87c4fd334a1d11458
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-01-26 12:41:22 +0100

    Mapped on wilders array

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,10 +18,9 @@ const Home: NextPage = () => {
       <main className="container">
         <h2>Wilders</h2>
         <section className="card-row">
-          <Wilder name={wilders[0].name} />
-          <Wilder name={wilders[1].name} />
-          <Wilder name={wilders[2].name} />
-          <Wilder name={wilders[3].name} />
+          {wilders.map((el) => (
+            <Wilder name={el.name} />
+          ))}
         </section>
       </main>
       <footer>
